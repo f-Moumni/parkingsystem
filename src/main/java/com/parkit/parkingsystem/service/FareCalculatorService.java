03838159e9923a3fee8e765fe574a5b012ca8bfd
fatima' <'fatima.moumni@hotmail.fr'>
2021-12-21 09:50:54 +0100

    add 30-min parking free

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,31 +1,46 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.Date;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null)
+				|| (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:"
+					+ ticket.getOutTime().toString());
+		}
+		double duration = getTheDurationToBePaid(ticket.getInTime(),
+				ticket.getOutTime());
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+		switch (ticket.getParkingSpot().getParkingType()) {
+			case CAR : {
+				ticket.setPrice(
+						Math.round((duration * Fare.CAR_RATE_PER_HOUR) * 100.0)
+								/ 100.0);
+				break;
+			}
+			case BIKE : {
+				ticket.setPrice(
+						Math.round((duration * Fare.BIKE_RATE_PER_HOUR) * 100.0)
+								/ 100.0);
+				break;
+			}
+			default :
+				throw new IllegalArgumentException("Unkown Parking Type");
+		}
+	}
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+	private double getTheDurationToBePaid(Date inTime, Date outTime) {
+		double duration = (outTime.getTime() - inTime.getTime()) / (1000 * 60);
+		if (duration <= 30) {
+			duration = 0;
+		}
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		return duration / 60;
+
+	}
 }
--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -1,12 +1,13 @@
 package com.parkit.parkingsystem;
 
-import com.parkit.parkingsystem.constants.ParkingType;
-import com.parkit.parkingsystem.dao.ParkingSpotDAO;
-import com.parkit.parkingsystem.dao.TicketDAO;
-import com.parkit.parkingsystem.model.ParkingSpot;
-import com.parkit.parkingsystem.model.Ticket;
-import com.parkit.parkingsystem.service.ParkingService;
-import com.parkit.parkingsystem.util.InputReaderUtil;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,48 +15,92 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Date;
-
-import static org.mockito.Mockito.*;
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingServiceTest {
 
-    private static ParkingService parkingService;
+	private static ParkingService parkingService;
 
-    @Mock
-    private static InputReaderUtil inputReaderUtil;
-    @Mock
-    private static ParkingSpotDAO parkingSpotDAO;
-    @Mock
-    private static TicketDAO ticketDAO;
+	@Mock
+	private static InputReaderUtil inputReaderUtil;
+	@Mock
+	private static ParkingSpotDAO parkingSpotDAO;
+	@Mock
+	private static TicketDAO ticketDAO;
 
-    @BeforeEach
-    private void setUpPerTest() {
-        try {
-            when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+	@BeforeEach
+	private void setUpPerTest() {
+		try {
 
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-            Ticket ticket = new Ticket();
-            ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));
-            ticket.setParkingSpot(parkingSpot);
-            ticket.setVehicleRegNumber("ABCDEF");
-            when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+			parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO,
+					ticketDAO);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Failed to set up test mock objects");
+		}
+	}
+	@Test
+	void getNextParkingNumberIfAvailableTest() {
+		ParkingType parkingType = ParkingType.CAR;
+		when(inputReaderUtil.readSelection()).thenReturn(1);
+		when(parkingSpotDAO.getNextAvailableSlot(parkingType)).thenReturn(1);
+		ParkingSpot parkingSpot = parkingService
+				.getNextParkingNumberIfAvailable();
 
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+		verify(parkingSpotDAO, times(1))
+				.getNextAvailableSlot(any(ParkingType.class));
+		verify(inputReaderUtil, times(1)).readSelection();
+		assertThat(parkingSpot.getParkingType()).isEqualTo(ParkingType.CAR);
+	}
 
-            parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw  new RuntimeException("Failed to set up test mock objects");
-        }
-    }
+	@Test
+	void processIncomingVehicleTest() throws Exception {
+		// given
+		when(inputReaderUtil.readSelection()).thenReturn(1);
+		ParkingType parkingType = ParkingType.CAR;
+		when(parkingSpotDAO.getNextAvailableSlot(parkingType)).thenReturn(1);
+		when(parkingSpotDAO.vehicleIsInParking("ABCD")).thenReturn(true);
+		ParkingSpot parkingSpot = new ParkingSpot(1, parkingType, true);
+		when(inputReaderUtil.readVehicleRegistrationNumber())
+				.thenReturn("ABCD");
+		// When
+		parkingService.processIncomingVehicle();
+		// THEN
+		verify(parkingSpotDAO, times(1)).updateParking(parkingSpot);
+		verify(ticketDAO, times(1)).saveTicket(any(Ticket.class));
+	}
 
-    @Test
-    public void processExitingVehicleTest(){
-        parkingService.processExitingVehicle();
-        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
-    }
+	@Test
+	void processExitingVehicleTest() throws Exception {
+		// given
 
+		when(inputReaderUtil.readVehicleRegistrationNumber())
+				.thenReturn("ABCDEF");
+
+		ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+		Ticket ticket = new Ticket();
+		ticket.setInTime(
+				new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
+		ticket.setParkingSpot(parkingSpot);
+		ticket.setVehicleRegNumber("ABCDEF");
+		when(parkingSpotDAO.updateParking(any(ParkingSpot.class)))
+				.thenReturn(true);
+		when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+		when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+
+		// When
+		parkingService.processExitingVehicle();
+		// THEN
+		verify(ticketDAO, times(1)).getTicket(anyString());
+		verify(parkingSpotDAO, Mockito.times(1))
+				.updateParking(any(ParkingSpot.class));
+		verify(ticketDAO, times(1)).updateTicket(ticket);
+	}
 }
